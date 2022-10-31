cf6f0e06ca75ca25641c270ce9b56dc23b955bac
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2022-10-31 15:11:57 -0300

    exercise-life-cycles

---
+++ secao4-life-cycles/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PersonalInfo from './components/PersonDetails'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+        <PersonalInfo/>
+    )
+  }
+}
+
+export default App;
+
+
